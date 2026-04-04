ef1087ac2a18c32a90c7fde4ae18a6d2270bb8c6
Karen Tsaturyan <karen_tsaturyan@epam.com>
2026-04-04 15:02:08 +0400

    Patch, delete tasks

--- internal/features/tasks/repository/postgres/models.go
+++ internal/features/tasks/repository/postgres/models.go
@@ -21,17 +21,21 @@ func taskDomainsFromModels(taskModels []TaskModel) []domain.Task {
 	domains := make([]domain.Task, len(taskModels))
 
 	for i, taskModel := range taskModels {
-		domains[i] = domain.NewTask(
-			taskModel.ID,
-			taskModel.Version,
-			taskModel.Title,
-			taskModel.Description,
-			taskModel.Completed,
-			taskModel.CreatedAt,
-			taskModel.CompletedAt,
-			taskModel.AuthorUserID,
-		)
+		domains[i] = taskDomainFromModel(taskModel)
 	}
 
 	return domains
+}
+
+func taskDomainFromModel(taskModel TaskModel) domain.Task {
+	return domain.NewTask(
+		taskModel.ID,
+		taskModel.Version,
+		taskModel.Title,
+		taskModel.Description,
+		taskModel.Completed,
+		taskModel.CreatedAt,
+		taskModel.CompletedAt,
+		taskModel.AuthorUserID,
+	)
 }
